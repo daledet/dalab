640b65f42b1fbde7e2e2e923665635d8d09eb510
Dwight Ledet <dwightledet@gmail.com>
2021-01-08 08:11:19 +0100

    correction

--- base/api.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://api.openweathermap.org/data/2.5/weather?id=3137115&"
-api = "appid=4fbcfd9147ff6df0e3ee3de039a4e70c&units=metric"
-
-response = requests.get(url, api)
-data = response.json()
-print(data)
--- base/config.py
+++
@@ -1,1 +0,0 @@
-api = "appid=4fbcfd9147ff6df0e3ee3de039a4e70c&units=metric"
